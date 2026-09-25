b3e5522ec31a25b1d63b65be487fc6fc00411cac
Roshan Patel <roshanpatel12309@gmail.com>
2026-09-25 14:57:10 +0530

    feat: helper functions

---
+++ backend/src/utils/validateUrl.ts
@@ -0,0 +1,11 @@
+export const isValidUrl = (value: string): boolean => {
+    try{
+        const url = new URL(value);
+
+        return (
+            url.protocol === "http:" || url.protocol === "https:"
+        );
+    } catch {
+        return false;
+    }
+};
